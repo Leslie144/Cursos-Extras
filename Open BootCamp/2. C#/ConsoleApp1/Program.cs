0d59a29ebacee17c1db57732546a22a72906787f
Leslie Tc <u201914358@upc.edu.pe>
2023-05-25 12:57:35 -0500

    Ejercicio 1 y 2

---
+++ Open BootCamp/2. C#/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿//EJERCICIO 1
+//Escribe un programa que reciba tu nombre y lo escriba por consola
+
+Console.WriteLine("Introduce tu nombre: ");
+string nombre = Console.ReadLine();
+
+
+Console.WriteLine("Hola, " + nombre);
+
+
+//EJERCICIO 2
+//Escribe un programa que tome la hora y escriba por pantalla
+// Obtener la hora actual del sistema
+DateTime horaActual = DateTime.Now;
+
+// Mostrar la hora en la consola
+Console.WriteLine("La hora actual es: " + horaActual.ToString("HH:mm:ss"));
