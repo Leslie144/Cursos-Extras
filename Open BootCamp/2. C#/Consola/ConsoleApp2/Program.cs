a572cd851c71f9541702e4ec6c7fd498ab09e170
Leslie Tc <u201914358@upc.edu.pe>
2023-05-25 16:36:15 -0500

    ejercicio 1 y 2

---
+++ Open BootCamp/2. C#/Consola/ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Solicitar datos de la persona
+
+//Ejercicio 1:
+Console.Write("Ingrese su nombre: ");
+string nombre = Console.ReadLine();
+
+Console.Write("Ingrese su apellido: ");
+string apellido = Console.ReadLine();
+
+Console.Write("Ingrese su edad: ");
+int edad = int.Parse(Console.ReadLine());
+
+Console.Write("¿Sabe programar? (Sí/No): ");
+bool sabeProgramar = Console.ReadLine().ToLower() == "sí";
+
+// Generar el mensaje
+string mensaje = "Bienvenido, " + nombre + " " + apellido + ". ";
+
+if (edad >= 18)
+{
+    mensaje += "Eres mayor de edad. ";
+}
+else
+{
+    mensaje += "Eres menor de edad. ";
+}
+
+if (sabeProgramar)
+{
+    mensaje += "Sabes programar.";
+}
+else
+{
+    mensaje += "No sabes programar.";
+}
+
+// Mostrar el mensaje
+Console.WriteLine(mensaje);
---
+++ Open BootCamp/2. C#/Consola/ConsoleApp4/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Solicitar datos de la persona
+
+//Ejercicio 2:
+// Coche
+int puertas = 4;            // Número de puertas (entero)
+int ruedas = 4;             // Número de ruedas (entero)
+string marca = "Toyota";    // Marca del coche (cadena de texto)
+bool itvVigente = true;     // ITV vigente (booleano)
+
+// Mesa
+double peso = 10.5;         // Peso de la mesa (decimal)
+double largo = 150.0;       // Largo de la mesa en cm (decimal)
+string material = "Madera"; // Material de la mesa (cadena de texto)
+string color = "Negro";     // Color de la mesa (cadena de texto)
